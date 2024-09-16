edde69662de902db82cfecce3317e8901e11b02d
Kamil Pluszczewicz <k.pluszczewicz@gmail.com>
2024-09-16 23:47:45 +0200

    Added copying last result to clipboard.

---
+++ tmux/scripts/copy-console-last-output.sh
@@ -0,0 +1,7 @@
+copy-mode
+send-keys -X search-backward '=>'
+send-keys -X next-word
+send-keys -X begin-selection
+send-keys -X end-of-line
+send-keys -X cursor-left
+send-keys -X copy-pipe-and-cancel pbcopy
