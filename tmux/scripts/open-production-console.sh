39f8668249b420530038051f3e5036446582043e
Kamil Pluszczewicz <kamil.pluszczewicz@astarium.pl>
2024-09-17 09:21:00 +0200

    Changes in tmux - conf and scripts.

---
+++ tmux/scripts/open-production-console.sh
@@ -0,0 +1,2 @@
+send-keys C-u ssh-production-110 Enter
+send-keys 'cd www/koleo/current && bin/rails c' Enter
---
+++ tmux/scripts/open-staging-console.sh
@@ -0,0 +1,2 @@
+send-keys -t 0 C-u ssh-staging Enter
+send-keys -t 0 'cd www/koleo/current && bin/rails c' Enter
