f042a5e1d2cab975242adb5dfd3a28ca7ccc2e1d
Kamil Pluszczewicz <k.pluszczewicz@gmail.com>
2024-11-29 22:11:14 +0100

    nvim: Added completion option for buffer to autocomplete words from visible buffers.

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -100,7 +100,18 @@ return {
 				sources = {
 					{ name = "nvim_lsp" },
 					{ name = "nvim_lsp_signature_help" },
-					{ name = "buffer" },
+					{
+						name = "buffer",
+						option = {
+							get_bufnrs = function()
+								local bufs = {}
+								for _, win in ipairs(vim.api.nvim_list_wins()) do
+									bufs[vim.api.nvim_win_get_buf(win)] = true
+								end
+								return vim.tbl_keys(bufs)
+							end,
+						},
+					},
 					{ name = "path" },
 					-- { name = "nvim_lua" },
 					{ name = "luasnip" },
