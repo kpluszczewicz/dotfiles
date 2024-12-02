495fd022ae0d385686715f848ca98d082b24681a
Kamil Pluszczewicz <kamil.pluszczewicz@astarium.pl>
2024-12-02 09:12:37 +0100

    Added 2 servers to tmux script/

--- tmux/scripts/open-production-ssh.sh
+++ tmux/scripts/open-production-ssh.sh
@@ -1,10 +1,14 @@
 new-window
 if-shell "wait 1" 'select-window -t "{last}"'
-split-window -h
-split-window -t 0 -v
-split-window -t 2 -v
+split-window -t 0 -v -l 30%
+split-window -t 0 -v -l 50%
+split-window -t 0 -h
+split-window -t 2 -h
+split-window -t 4 -h
 send-keys -t 0 ssh-production-110 Enter
 send-keys -t 1 ssh-production-111 Enter
-send-keys -t 2 ssh-production-210 Enter
-send-keys -t 3 ssh-production-211 Enter
+send-keys -t 2 ssh-production-112 Enter
+send-keys -t 3 ssh-production-113 Enter
+send-keys -t 4 ssh-production-210 Enter
+send-keys -t 5 ssh-production-211 Enter
 set-option synchronize-panes on
