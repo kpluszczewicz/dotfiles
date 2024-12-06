0894240cf75d4c45046dbc045d67e2d22648f64b
Kamil Pluszczewicz <kamil.pluszczewicz@astarium.pl>
2024-12-06 10:26:14 +0100

    Tmux: corrected script to open prod servers (added new servers).

--- tmux/scripts/open-production-ssh.sh
+++ tmux/scripts/open-production-ssh.sh
@@ -1,14 +1,18 @@
 new-window
 if-shell "wait 1" 'select-window -t "{last}"'
-split-window -t 0 -v -l 30%
-split-window -t 0 -v -l 50%
-split-window -t 0 -h
+split-window -t 0 -v
+split-window -t 1 -v
+split-window -t 0 -v
+split-window -t 3 -h
 split-window -t 2 -h
-split-window -t 4 -h
+split-window -t 1 -h
+split-window -t 0 -h
 send-keys -t 0 ssh-production-110 Enter
 send-keys -t 1 ssh-production-111 Enter
 send-keys -t 2 ssh-production-112 Enter
 send-keys -t 3 ssh-production-113 Enter
 send-keys -t 4 ssh-production-210 Enter
 send-keys -t 5 ssh-production-211 Enter
+send-keys -t 6 ssh-production-212 Enter
+send-keys -t 7 ssh-production-213 Enter
 set-option synchronize-panes on
