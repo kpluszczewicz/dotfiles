f887b0689a399017ef1a55184bd75c90582c71d5
Kamil Pluszczewicz <kamil.pluszczewicz@astarium.pl>
2024-12-06 10:25:55 +0100

    Tmux: corrected script to copy last ruby output.

--- tmux/scripts/copy-console-last-output.sh
+++ tmux/scripts/copy-console-last-output.sh
@@ -1,7 +1,9 @@
 copy-mode
-send-keys -X search-backward '=>'
-send-keys -X next-word
-send-keys -X begin-selection
-send-keys -X end-of-line
+send-keys -X cursor-up
 send-keys -X cursor-left
-# send-keys -X copy-pipe-and-cancel pbcopy
+send-keys -X begin-selection
+send-keys -X start-of-line
+send-keys -X cursor-right
+send-keys -X cursor-right
+send-keys -X cursor-right
+send-keys -X copy-pipe-and-cancel pbcopy # NOTE: Zwykłe copy-selection wrzuci tekst do wewnętrznego schowka tmux'a
