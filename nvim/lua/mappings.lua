0f374f11825c923ff5dc9d54bf0d78c655dd921d
Kamil Pluszczewicz <kamil.pluszczewicz@astarium.pl>
2024-12-12 09:31:29 +0100

    (nvim/lua/mappings.lua) Added keybiding C-r in visual mode for s/<selected text>/cursor.

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -88,3 +88,22 @@ end, { desc = "Tag current <cword>" })
 -- emoji.nvim
 vim.keymap.set("i", "<C-t>e", require("emoji").insert, { desc = "[S]earch [E]moji" })
 vim.keymap.set("n", "<leader>se", require("emoji").insert, { desc = "[S]earch [E]moji" })
+
+-- TODO: Przeanalizować i zrozumieć poniższy kod.
+-- get contents of visual selection
+-- handle unpack deprecation
+-- from https://stackoverflow.com/questions/676600/vim-search-and-replace-selected-text
+table.unpack = table.unpack or unpack
+local function get_visual()
+	local _, ls, cs = table.unpack(vim.fn.getpos("v"))
+	local _, le, ce = table.unpack(vim.fn.getpos("."))
+	return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
+end
+
+vim.keymap.set("v", "<C-r>", function()
+	local pattern = table.concat(get_visual())
+	-- escape regex and line endings
+	pattern = vim.fn.substitute(vim.fn.escape(pattern, "^$.*\\/~[]"), "\n", "\\n", "g")
+	-- send parsed substitution command to command line
+	vim.api.nvim_input("<Esc>:%s/" .. pattern .. "//<Left>")
+end)
